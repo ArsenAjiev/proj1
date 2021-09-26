ae0b780cc2b7be4ce475b405c92082630016e350
ArsenAjiev <selftcoder@gmail.com>
2021-09-26 12:06:32 +0300

    add host

--- proj1/settings.py
+++ proj1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^#64^j%dhoixlxmht91b$w()_lk3eg1x2sg@+sez@y545%3owk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'mytestingsapp.herokuapp.com']
 
 
 # Application definition
